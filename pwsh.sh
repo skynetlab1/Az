50c9d1ceb10f0f8108ae85a1e1ba5bb29234df3e
Sky Soft Bulgaria <expertpcin@gmail.com>
2024-06-26 23:23:08 +0300

    Update pwsh.sh

--- pwsh.sh
+++ pwsh.sh
@@ -1,22 +1,14 @@
-#!/bin/bash
-
 # Update the list of packages
-sudo apt-get update -y
-
-# Install pre-requisite packages
+sudo apt-get update
+# Install prerequisite packages
 sudo apt-get install -y wget apt-transport-https software-properties-common
-
-# Download the Microsoft repository GPG keys
-wget -q https://packages.microsoft.com/config/ubuntu/24.04/packages-microsoft-prod.deb
-
-# Register the Microsoft repository GPG keys
+# Download the Microsoft repository keys
+wget -q https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb
+# Register the Microsoft repository keys
 sudo dpkg -i packages-microsoft-prod.deb
-
-# Update the list of products
-sudo apt-get update -y
-
-# Enable the "universe" repositories
-sudo add-apt-repository -y noble
-
+# Update the package list
+sudo apt-get update
 # Install PowerShell
 sudo apt-get install -y powershell
+# Start PowerShell
+pwsh
