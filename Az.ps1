8dcb48f27e43842f86481c727c1308131ba70237
Sky Soft Bulgaria <expertpcin@gmail.com>
2024-06-27 06:16:37 +0300

    Update Az.ps1

--- Az.ps1
+++ Az.ps1
@@ -1,12 +1,6 @@
-# Set PSGallery as trusted
-Write-Output "Setting PSGallery as trusted..."
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-
-# Sign in to your Azure subscription
 $sub = Get-AzSubscription -ErrorAction SilentlyContinue
 if (-not $sub) {
   Connect-AzAccount  -Identity
 }
 
-# If you have multiple subscriptions, set the one to use
 Select-AzSubscription -SubscriptionId $sub.Id
